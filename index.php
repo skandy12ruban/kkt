9de47ad16a074c95e775708dc87e1cd2ecd3a8ab
skandy12ruban <skandyruban@gmail.com>
2017-03-21 20:27:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+include("PHPmailer/PHPMailerAutoload.php");
+$errEmail = "";
+$errHuman = "";
+$errMessage = "";
+$errName = "";
+$result = "";
+$email = "";
+$message = "";
+$name = "";
+$account = 'skandyssn@gmail.com';
+$password = 'subashree';
+$from = 'klickkit@gmail.com';
+$from_name = 'klickkit';
+$subject = 'Response to your mail';
+$msg = 'this is to response your email.We will get back to you soon.';
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$human = intval($_POST['human']);
+		$message = $_POST['message'];
+		$subject = "Reply to $name for $message";
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth= true;
+$mail->Port = 465; // Or 587
+$mail->Username= $account;
+$mail->Password= $password;
+$mail->SMTPSecure = 'ssl';
+$mail->From = $from;
+$mail->FromName= $from_name;
+$mail->isHTML(true);
+$mail->Subject = $subject;
+$mail->Body = $msg;
+$mail->addAddress($email);
+if(!$mail->send()){
+ $result = $mail->ErrorInfo;
+}else{
+ $result = "Your Email sent succesfully.";
+}
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact Form </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<style>
+	body {
+    background: url('http://www.lifewallpapers.net/data/out/75/4170169-color-gradient-wallpaper-hd.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    background-size: cover;
+    -o-background-size: cover;
+}
+	</style>
+  </head>
+  <body>
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-md-6 col-md-offset-3">
+  				<h1 class="page-header text-center">Contact Form Example</h1>
+				<form class="form-horizontal" role="form" method="post" action="index.php">
+					<div class="form-group">
+						<label for="name" class="col-sm-2 control-label">Name</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name">
+							<?php echo "<p class='text-danger'>$errName</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email" class="col-sm-2 control-label">Email</label>
+						<div class="col-sm-10">
+							<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com">
+							<?php echo "<p class='text-danger'>$errEmail</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message" class="col-sm-2 control-label">Message</label>
+						<div class="col-sm-10">
+							<textarea class="form-control" rows="4" name="message"></textarea>
+							<?php echo "<p class='text-danger'>$errMessage</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<?php echo $result; ?>	
+						</div>
+					</div>
+				</form> 
+			</div>
+		</div>
+	</div>   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </body>
+  
+</html>
